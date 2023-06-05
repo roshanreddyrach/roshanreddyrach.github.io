31a0cba79ae231a61543248d12c534352d1babd5
ROSHAN REDDY RACHAMALLA <roshanrithik279@gmail.com>
2023-06-05 19:39:53 +0530

    new mailing feature integrated

--- script.js
+++ script.js
@@ -123,3 +123,34 @@ scrollBottom.forEach((el) => observer.observe(el))
 
 const scrollTop = document.querySelectorAll(".scroll-top")
 scrollTop.forEach((el) => observer.observe(el))
+
+
+// Email Send ///////////////////////////////////////
+
+
+function sendMail(){
+    var params = {
+        name : document.getElementById("name").value ,
+        email : document.getElementById("email").value ,
+        contact : document.getElementById("phoneNumber").value ,
+        subject : document.getElementById("subject").value ,
+        message : document.getElementById("message").value ,
+
+    }
+
+    const serviceID = "service_6wk852t"
+    const templateID = "template_2svauwu"
+
+    emailjs.send(serviceID,templateID,params)
+    .then((res) => {
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("phoneNumber").value = "";
+          document.getElementById("subject").value = "";
+          document.getElementById("message").value= "";
+          console.log(res);
+          alert("your message sent successfully");
+        })
+        .catch((err) => console.log(err))
+  }
+
